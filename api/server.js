854f45b64b173aa6853837f990bbe5536b36063b
Salim Çobanoğlu <salimcobanoglu@yahoo.com>
2023-05-17 17:32:33 +0300

    first installation is done

--- api/server.js
+++ api/server.js
@@ -1,3 +1,12 @@
 // server için gerekli olanları burada ayarlayın
 
 // posts router'ını buraya require edin ve bağlayın
+
+const express = require("express");
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Server is up and running!!!...");
+});
+
+module.exports = server;
